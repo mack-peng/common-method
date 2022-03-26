cc922335faa7a392948e62e9984ad531f84a98f5
76wenshu <1210238599@qq.com>
2022-03-26 10:03:11 +0800

    npm包发布文件

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+import float from './common/float.js'
+import digitalThousandthProcessing from './common/./common/digitalThousandthProcessing.js'
+import timeWithinYear from './common/timeWithinYear.js'
+import comparesContant from './common/comparesContant.js'
+
+export {
+    float,digitalThousandthProcessing,timeWithinYear,comparesContant
+}
